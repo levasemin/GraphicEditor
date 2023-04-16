06545d9c33b1eadf9533c6269e6e1eb6b8cbfeb7
levasemin <62355489+levasemin@users.noreply.github.com>
2023-04-16 15:13:11 +0300

    Delete src directory

--- src/Library/Font.cpp
+++
@@ -1,5 +0,0 @@
-#include "Font.h"
-
-const char *TIMES_NEW_ROMAN_FONT = "fonts/Times_New_Roman.ttf";
-
-const Font Font::Times_new_roman (TIMES_NEW_ROMAN_FONT);
--- src/Library/functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <math.h>
-
-float get_determinant(float *array, int n);
-int doublecmp(float number1, float number2);
-
-std::pair<float, float> solve_quadtratic(float a, float b, float c);
